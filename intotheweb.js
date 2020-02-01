2342501bf11d30784fde6875d51fa4f1ad8f2378
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-02-01 11:12:04 +0530

    debugging

--- intotheweb.js
+++ intotheweb.js
@@ -1,15 +1,27 @@
 var host = "https://navquery.com";
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)[1]};
-    },
-    {urls: [
-        "*://facebook.com/*",
-        "*://www.facebook.com/*",
-        "*://yahoo.com/*",
-        "*//search.yahoo.com/*",
+  function(details) {
+    return {
+      redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]
+    };
+  },
+  {
+    urls: [
+      "*://facebook.com/*",
+      "*://www.facebook.com/*",
+      "*://yahoo.com/*",
+      "*//search.yahoo.com/*"
     ],
-    types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
-},
-    ["blocking"]
+    types: [
+      "main_frame",
+      "sub_frame",
+      "stylesheet",
+      "script",
+      "image",
+      "object",
+      "xmlhttprequest",
+      "other"
+    ]
+  },
+  ["blocking"]
 );
